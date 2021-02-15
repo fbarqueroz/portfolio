932b6c60ae012d74768ea325a8bb6ed9dbb01321
fbarqueroz <65320282+fbarqueroz@users.noreply.github.com>
2021-02-14 20:26:26 -0600

    Agrego estructuras html, js y estilos

---
+++ js/script.js
@@ -0,0 +1,75 @@
+/* eslint-disable quotes */
+/* eslint-disable linebreak-style */
+
+function agregarMensajeDeError(camposInvalidos) {
+  const errorElemnt = document.createElement("div");
+  errorElemnt.classList.add("mensaje-error");
+  // AQUI VAMOS A ESCRIBIR EL MENSAJE DE ERROR
+  const mensajeInvalidoTitulo = document.createElement("h2");
+  mensajeInvalidoTitulo.innerText = 'Ocurrio un error, verifica los siguientes campos:';
+
+  const listaInvalidoInput = document.createElement("ul");
+
+  camposInvalidos.forEach((elementInvalido) => {
+    const li = document.createElement("li");
+    li.innerText = elementInvalido.getAttribute("name");
+
+    listaInvalidoInput.appendChild(li);
+  });
+
+  errorElemnt.appendChild(mensajeInvalidoTitulo);
+  errorElemnt.appendChild(listaInvalidoInput);
+
+  form.parentNode.insertBefore(errorElemnt, form);
+}
+
+function agregarMensajeDeExito() {
+  const validoElemnt = document.createElement("div");
+  validoElemnt.classList.add("mensaje-valido");
+
+  validoElemnt.innerText = "Su formulario fue enviado";
+
+  document.body.appendChild(validoElemnt);
+}
+
+function dameLosCamposInvalidos(inputsRequeridos) {
+  const invalidos = [];
+
+  inputsRequeridos.forEach((actualInput) => {
+    if (actualInput.value === "") {
+      invalidos.push(actualInput);
+      actualInput.style.border = "1px solid red";
+    } else {
+      actualInput.style.border = "";
+    }
+  });
+
+  return invalidos;
+}
+
+function reiniciarMensajesDeError() {
+  const mensajeDeErrror = document.querySelector(".mensaje-error");
+  if (mensajeDeErrror) {
+    mensajeDeErrror.remove();
+  }
+
+  const mensajeDeExito = document.querySelector(".mensaje-valido");
+  if (mensajeDeExito) {
+    mensajeDeExito.remove();
+  }
+}
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+  reiniciarMensajesDeError();
+  const inputsRequeridos = document.querySelectorAll(".requerido");
+  e.preventDefault();
+  const invalidos = dameLosCamposInvalidos(inputsRequeridos);
+  // Si tenemos campos invalidos
+  if (invalidos.length > 0) {
+    agregarMensajeDeError(invalidos);
+  } else {
+    agregarMensajeDeExito();
+  }
+});
